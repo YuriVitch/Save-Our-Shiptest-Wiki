2e864c10321ed2322fc4f07892b1f68dee4bbd61
YuriVitch <Yurivitch@hotmail.com>
2023-02-16 22:14:42 -0500

    Adds a basic home page and the rules

---
+++ Main.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="interface.css" />
+	</head>
+	<body>
+	</body>
+	<script type="text/javascript" src="main.js"></script>
+</html>
